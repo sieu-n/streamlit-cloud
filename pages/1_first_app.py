af5b2406049a74e5fccf4d009452d9faec28c01f
krenerd <sieunpark77@gmail.com>
2024-02-23 15:28:29 +0900

    change entry file

--- pages/1_first_app.py
+++ pages/1_first_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="First App", page_icon="📈")
+
 # Set the page title
 st.title('Welcome to My First App')
 
--- pages/2_second_page.py
+++ pages/2_second_page.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(page_title="Second App", page_icon="📈")
 
 # Set the page title
 st.title('Welcome to My second App')
